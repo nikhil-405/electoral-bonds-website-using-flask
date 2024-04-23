c7c922eab53bd2a5bc29548e18f053e51efbf4c7
Nikhil Goyal <148058602+nikhil-405@users.noreply.github.com>
2024-04-23 12:45:57 +0530

    Update assignment.py

--- Flask_Web_dev_assignment/assignment.py
+++ Flask_Web_dev_assignment/assignment.py
@@ -13,8 +13,8 @@ from flask_mysqldb import MySQL
 app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'nikhil'
+app.config['MYSQL_USER'] = ''
+app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'dcc_assignment'
 
 mysql = MySQL(app)
